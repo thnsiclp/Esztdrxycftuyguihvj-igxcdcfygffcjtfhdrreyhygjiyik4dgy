fe825c8872c839356e21b8874d0971dd093487cb
thnsiclp <121228953+thnsiclp@users.noreply.github.com>
2024-01-15 23:05:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
+// サーバーサイド
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
     });
 });
 
-server.listen(3000, () => {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
